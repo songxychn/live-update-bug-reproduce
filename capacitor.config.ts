22981ee37f9dd351eb445896459de9c4323f15e9
songxy <songxychina@gmail.com>
2024-04-19 23:39:26 +0800

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type {CapacitorConfig} from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'live-update-bug-reproduce',
+  webDir: 'dist'
+};
+
+export default config;
